c5038597a3393f1ef160614ea03eaf1621275c30
Meaglin <meaglin.wasabi@gmail.com>
2013-11-12 22:45:26 +0100

    Update sql file.

--- structure.sql
+++ structure.sql
@@ -1,25 +1,33 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*
+MySQL Backup
+Source Server Version: 5.5.32
+Source Database: sms
+Date: 11/12/2013 22:44:15
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+SET FOREIGN_KEY_CHECKS=0;
 
-
-CREATE TABLE IF NOT EXISTS `categories` (
+-- ----------------------------
+--  Table structure for `categories`
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parentid` int(11) NOT NULL DEFAULT '0',
-  `name` varchar(255) NOT NULL,
-  `displayname` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `description` text NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `displayname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `parentid_2` (`parentid`,`name`),
   KEY `parentid` (`parentid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=59 ;
+) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `files` (
+-- ----------------------------
+--  Table structure for `files`
+-- ----------------------------
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` bigint(20) NOT NULL,
   `modified_at` bigint(20) NOT NULL,
@@ -28,65 +36,78 @@ CREATE TABLE IF NOT EXISTS `files` (
   `categoryid` int(11) NOT NULL,
   `flag` int(11) NOT NULL,
   `duplicate` int(1) NOT NULL DEFAULT '0',
-  `name` varchar(255) NOT NULL,
-  `displayname` varchar(255) NOT NULL,
-  `directory` varchar(255) NOT NULL,
-  `displaydirectory` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `extension` varchar(255) NOT NULL,
-  `path` text NOT NULL,
-  `serverpath` text NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `displayname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `directory` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `displaydirectory` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `extension` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `path` text CHARACTER SET utf8 NOT NULL,
+  `serverpath` text CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`),
   KEY `servercategoryid` (`servercategoryid`),
   KEY `serverid` (`serverid`),
   KEY `categoryid` (`categoryid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=140508 ;
+) ENGINE=InnoDB AUTO_INCREMENT=28805098 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `history` (
+-- ----------------------------
+--  Table structure for `history`
+-- ----------------------------
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE `history` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `time` bigint(20) NOT NULL,
-  `level` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `action` varchar(255) NOT NULL,
-  `serverid` int(11) NOT NULL,
-  `servercategoryid` int(11) NOT NULL,
-  `categoryid` int(11) NOT NULL,
-  `fileid` int(11) NOT NULL,
-  `item` text NOT NULL,
+  `level` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `action` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `item` text CHARACTER SET utf8 NOT NULL,
+  `serverid` int(10) NOT NULL,
+  `categoryid` int(10) NOT NULL,
+  `servercategoryid` int(10) NOT NULL,
+  `fileid` int(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB AUTO_INCREMENT=69599 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `servercategories` (
+-- ----------------------------
+--  Table structure for `servercategories`
+-- ----------------------------
+DROP TABLE IF EXISTS `servercategories`;
+CREATE TABLE `servercategories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `categoryid` int(11) NOT NULL,
   `serverid` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `path` text NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `path` text CHARACTER SET utf8 NOT NULL,
+  `dontscan` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `categoryid` (`categoryid`,`serverid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=65 ;
+) ENGINE=InnoDB AUTO_INCREMENT=137 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `servers` (
+-- ----------------------------
+--  Table structure for `servers`
+-- ----------------------------
+DROP TABLE IF EXISTS `servers`;
+CREATE TABLE `servers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `displayname` varchar(255) NOT NULL,
-  `code` varchar(4) NOT NULL,
-  `description` text NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `config` text NOT NULL,
-  `status` varchar(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `displayname` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `code` varchar(4) CHARACTER SET utf8 NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `config` text CHARACTER SET utf8 NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8 NOT NULL,
   `disconnectcount` int(10) NOT NULL DEFAULT '0',
   `disconnected` int(1) NOT NULL DEFAULT '0',
   `enabled` int(10) NOT NULL DEFAULT '1',
   `lastupdate` bigint(20) NOT NULL,
   `lastchange` bigint(20) NOT NULL,
-  `ip` varchar(255) NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8 NOT NULL,
   `filecount` int(11) NOT NULL,
   `categorycount` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+--  Records 
+-- ----------------------------
